baf3c07e3f063c43b19646e38f78e9d189faaef9
Kaioru <keith@kaioru.co>
2019-12-02 11:53:45 +0800

    Add history view

--- todofy/apps/todo/urls.py
+++ todofy/apps/todo/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='todo_index'),
+    path("history/", views.history, name='todo_history'),
     path("create/", views.create, name='todo_create'),
     path("<int:todo>/archive/", views.archive, name="todo_archive"),
     path("<int:todo>/delete/", views.delete, name="todo_delete"),
