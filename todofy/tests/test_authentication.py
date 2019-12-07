e90d53d5ed025900a544261c798944dc2213da63
Daniel Ian Dean <danieliandean2@gmail.com>
2019-12-08 01:32:55 +0800

    Update authentication test cases

--- todofy/tests/test_authentication.py
+++ todofy/tests/test_authentication.py
@@ -2,29 +2,27 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-def user_successful_login(driver):
+def test_user_successful_login(driver):
     driver.get("http://127.0.0.1:8000/accounts/login")
     name = driver.find_element_by_name("username")
     password = driver.find_element_by_name("password")
-    submit = driver.find_element_by_xpath("//input[@value='Log In']")
-    name.send_keys("123@abc.com")
+    submit = driver.find_element_by_xpath("//button[@type='submit']")
+    name.send_keys("johndoe")
+    password.send_keys("123123123d")
+    submit.click()
+
+    breadcrumb = driver.find_element_by_xpath("//li[@class='breadcrumb-item active']")
+        
+    assert breadcrumb.text == "Todo"
+
+
+def test_user_unsuccessful_login(driver):
+    driver.get("http://127.0.0.1:8000/accounts/login")
+    name = driver.find_element_by_name("username")
+    password = driver.find_element_by_name("password")
+    submit = driver.find_element_by_xpath("//button[@type='submit']")
+    name.send_keys("bobber")
     password.send_keys("123123")
     submit.click()
 
-def user_unsuccessful_login(driver):
-    driver.get("http://127.0.0.1:8000/accounts/login")
-    name = driver.find_element_by_name("username")
-    password = driver.find_element_by_name("password")
-    submit = driver.find_element_by_xpath("//input[@value='Log In']")
-    name.send_keys("122@abb.com")
-    password.send_keys("111111")
-    submit.click()
-
-def user_login_syntax_error(driver):
-    driver.get("http://127.0.0.1:8000/accounts/login/")
-    name = driver.find_element_by_name("username")
-    password = driver.find_element_by_name("password")
-    submit = driver.find_element_by_xpath("//input[@value='Log In']")
-    name.send_keys("123gmail.com")
-    password.send_keys("111111")
-    submit.click()
+    assert "Sign In" in driver.title
