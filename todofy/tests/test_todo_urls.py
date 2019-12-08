991818174446dc9f93fad502a3f76cb8e5281c47
Daniel Ian Dean <danieliandean2@gmail.com>
2019-12-08 11:18:13 +0800

    Create test cases for todo page features

--- todofy/tests/test_todo_urls.py
+++ todofy/tests/test_todo_urls.py
@@ -1,13 +1,13 @@
-# from django.urls import reverse
+from django.urls import reverse
 
 
-# def test_todo_url(client, live_server):
-#     response = client.get(live_server.url + reverse('todo_index'))
+def test_todo_url(client, live_server):
+    response = client.get(live_server.url + reverse('todo_index'))
 
-#     assert response.status_code == 200
+    assert response.status_code == 200
 
 
-# def test_todo_history_url(client, live_server):
-#     response = client.get(live_server.url + reverse('todo_history'))
+def test_todo_history_url(client, live_server):
+    response = client.get(live_server.url + reverse('todo_history'))
 
-#     assert response.status_code == 200
+    assert response.status_code == 200
