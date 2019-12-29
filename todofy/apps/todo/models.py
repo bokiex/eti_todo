196ccd8340fa39b8f83e1e4dafb45beb8e87248d
Kaioru <keith@kaioru.co>
2019-12-29 12:01:32 +0800

    Add filter for authed users

--- todofy/apps/todo/models.py
+++ todofy/apps/todo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Todo(models.Model):
@@ -6,6 +7,8 @@ class Todo(models.Model):
     archived = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         get_latest_by = 'created_at'
--- todofy/apps/todo/views.py
+++ todofy/apps/todo/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from .forms import TodoForm
 from .models import Todo
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
-    items = Todo.objects.filter(archived=False).order_by('-created_at')
+    items = Todo.objects.filter(user=request.user, archived=False).order_by('-created_at')
     return render(request, 'todo-index.html', {'items': items, 'form': TodoForm()})
 
 
+@login_required
 def create(request):
     if request.method == "POST":
         form = TodoForm(request.POST)
@@ -14,12 +17,14 @@ def create(request):
         if form.is_valid():
             item = Todo(
                 content=form.cleaned_data['content'],
-                archived=False
+                archived=False,
+                user=request.user
             )
             item.save()
     return redirect('todo_index')
 
 
+@login_required
 def archive(request, todo):
     item = Todo.objects.get(id=todo)
     item.archived = True
@@ -27,12 +32,14 @@ def archive(request, todo):
     return redirect('todo_index')
 
 
+@login_required
 def delete(request, todo):
     item = Todo.objects.get(id=todo)
     item.delete()
     return redirect('todo_index')
 
 
+@login_required
 def history(request):
-    items = Todo.objects.order_by('-created_at')
+    items = Todo.objects.filter(user=request.user).order_by('-created_at')
     return render(request, 'todo-history.html', {'items': items})
