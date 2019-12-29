a1d865bc4ad9da8b3ee6e145bf3ec45480ce6a30
Kaioru <keith@kaioru.co>
2019-12-29 17:50:27 +0800

    Adjust test cases to match new requirements

--- todofy/tests/test_view_navbar.py
+++ todofy/tests/test_view_navbar.py
@@ -5,7 +5,21 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-def test_view_navbar_index(driver, live_server):
+@pytest.fixture
+def login_page(driver, live_server, admin_user):
+    driver.get(live_server.url + "/accounts/login")
+
+    name = driver.find_element_by_name("username")
+    password = driver.find_element_by_name("password")
+    submit = driver.find_element_by_xpath("//button[@type='submit']")
+
+    name.send_keys("admin")
+    password.send_keys("password")
+
+    submit.click()
+
+
+def test_view_navbar_index(driver, live_server, login_page):
     driver.get(live_server.url + reverse('todo_index'))
 
     # nav = driver.find_element_by_class_name('navbar-nav')
@@ -17,7 +31,7 @@ def test_view_navbar_index(driver, live_server):
     wait.until(EC.url_matches(live_server.url + reverse('todo_index')))
 
 
-def test_view_navbar_history(driver, live_server):
+def test_view_navbar_history(driver, live_server, login_page):
     driver.get(live_server.url + reverse('todo_index'))
 
     # nav = driver.find_element_by_class_name('navbar-nav')
--- todofy/tests/test_view_todo_history.py
+++ todofy/tests/test_view_todo_history.py
@@ -7,14 +7,29 @@ from todofy.apps.todo.models import Todo
 
 
 @pytest.fixture
-def archived_todo():
-    todo = Todo(content='Buy groceries!', archived=True)
+def login_page(driver, live_server, admin_user):
+    driver.get(live_server.url + "/accounts/login")
+
+    name = driver.find_element_by_name("username")
+    password = driver.find_element_by_name("password")
+    submit = driver.find_element_by_xpath("//button[@type='submit']")
+
+    name.send_keys("admin")
+    password.send_keys("password")
+
+    submit.click()
+    
+
+
+@pytest.fixture
+def archived_todo(admin_user):
+    todo = Todo(content='Buy groceries!', user=admin_user, archived=True)
     todo.save()
 
     return todo
 
 
-def test_view_todo_history_populated(driver, live_server, archived_todo):
+def test_view_todo_history_populated(driver, live_server, archived_todo, login_page):
     driver.get(live_server.url + reverse('todo_history'))
 
     (WebDriverWait(driver, 3)
